82d4c242c05092df27f5ebd2bafe633c8ed687f9
mthalima <matheus.ecomp@gmail.com>
2023-11-30 17:33:54 -0300

    dkaspdkoa

--- components/Timer.jsx
+++ components/Timer.jsx
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
+import { Colors } from "../styles/Colors";
 
 const Timer = () => {
-  const [seconds, setSeconds] = useState(0);
+  const [seconds, setSeconds] = useState(1500);
   const [isActive, setIsActive] = useState(false);
 
+  //UseEffect com a logica do tempo do timer
   useEffect(() => {
     let interval;
 
     if (isActive) {
       interval = setInterval(() => {
-        setSeconds((prevSeconds) => prevSeconds + 1);
+        setSeconds((prevSeconds) => prevSeconds - 1);
       }, 1000);
     } else {
       clearInterval(interval);
@@ -30,18 +32,21 @@ const Timer = () => {
 
   const formatTime = (time) => {
     const pad = (val) => (val < 10 ? `0${val}` : val);
-    const hours = Math.floor(time / 3600);
     const minutes = Math.floor((time % 3600) / 60);
     const seconds = Math.floor(time % 60);
-    return `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
+    return `${pad(minutes)}:${pad(seconds)}`;
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.timer}>{formatTime(seconds)}</Text>
       <View style={styles.buttonContainer}>
-        <Button title={isActive ? "Pause" : "Start"} onPress={toggleTimer} />
-        <Button title="Reset" onPress={resetTimer} />
+        <Button
+          style={styles.timerButton}
+          title={isActive ? "Pause" : "Start"}
+          onPress={toggleTimer}
+        />
+        <Button style={styles.timerButton} title="Reset" onPress={resetTimer} />
       </View>
     </View>
   );
@@ -49,18 +54,26 @@ const Timer = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "green",
   },
   timer: {
-    fontSize: 48,
+    fontSize: 64,
     marginBottom: 20,
+    color: "#FDF7E4",
   },
   buttonContainer: {
     flexDirection: "row",
     justifyContent: "space-around",
+    alignContent: "center",
     width: "60%",
+  },
+
+  timerButton: {
+    backgroundColor: Colors.tomato,
+    color: Colors.tomato,
+    width: 20,
   },
 });
 
---
+++ styles/Colors.js
@@ -0,0 +1,3 @@
+export const Colors = {
+  tomato: "#f34e32",
+};
