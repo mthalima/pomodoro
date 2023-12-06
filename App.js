9eedde23152684092771880dfc6f3a7633627860
mthalima <matheus.ecomp@gmail.com>
2023-12-05 23:17:14 -0300

    criado a navegacao

--- App.js
+++ App.js
@@ -2,15 +2,18 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import RestTime from "./components/RestTime";
 import PomodoreScreen from "./screens/pomodoreScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <>
-      <PomodoreScreen>
-        <RestTime />
-      </PomodoreScreen>
-      <StatusBar style="auto" />
-    </>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Pomodore" component={PomodoreScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
