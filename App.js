30b855bee1f8e61ca7fd5e65de98bfa8c3ccba7a
matheus <matheus.ecomp@gmail.com>
2024-01-30 14:16:16 -0300

    refatored

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import RestTime from "./components/RestTime";
 import PomodoreScreen from "./screens/pomodoreScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
